80a5a17af302a95459fb8b44ffd1223388c0d64b
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-13 14:05:49 +0800

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Kelvin on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
